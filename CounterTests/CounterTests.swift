49702128e27591ab6c4d0a61a5fa5d34539fcf82
nex77qoz <nex77qoz@gmail.com>
2024-06-24 23:47:12 +0300

    Initial Commit

---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by Максим Бабкин on 24.06.2024.
+//
+
+import Testing
+@testable import Counter
+
+struct CounterTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
